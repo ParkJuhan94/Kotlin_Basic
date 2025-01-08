ea54caed435de985115ca97df058373bab70b642
ParkJuHan94 <1004sunye@naver.com>
2025-01-09 02:08:32 +0900

    1. var, val 변수 타입 2. 톱 레벨 상수 const

---
+++ src/KOTLIN_BASIC/Main_01.kt
@@ -0,0 +1,15 @@
+package KOTLIN_BASIC
+
+const val num = 20  // main 보다 먼저 컴파일 되는
+
+fun main() {
+    var i = 10
+    var name = "주주"
+    var point = 5.5
+
+    var j : Int = 10
+    var loc : String = "서울시"
+
+    j = 20 // 변수는 재 대입이 가능
+
+}
