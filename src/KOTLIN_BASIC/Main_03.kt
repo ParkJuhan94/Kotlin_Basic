8bba7dbef08bffb1a5a4016b2f9e124cde6442b0
ParkJuHan94 <1004sunye@naver.com>
2025-01-11 09:51:27 +0900

    Stirng 의 다양한 사용 예제

---
+++ src/KOTLIN_BASIC/Main_03.kt
@@ -0,0 +1,11 @@
+package KOTLIN_BASIC
+
+fun main() {
+    var name = "ZooPuter"
+
+    println(name.uppercase())
+    println(name.lowercase())
+    println(name[0])
+    println("제 이름은 " + name + "입니다.")
+    println("제 이름은 ${name + "님"} 입니다.")
+}
