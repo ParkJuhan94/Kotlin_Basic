dc476f84ba17082fae72cfba46860e3b4b0cc760
ParkJuHan94 <1004sunye@naver.com>
2025-01-12 03:41:50 +0900

    random 의 예제

--- src/KOTLIN_BASIC/Main_05.kt
+++ src/KOTLIN_BASIC/Main_05.kt
@@ -1,5 +1,14 @@
 package KOTLIN_BASIC
 
-fun main() {
+import kotlin.random.Random
 
+fun main() {
+    var randomIntNum = Random.nextInt()
+    println(randomIntNum)
+
+    randomIntNum = Random.nextInt(0, 100)
+    println(randomIntNum)
+
+    var randomDoubleNum = Random.nextDouble(0.0, 1.0)
+    println(randomDoubleNum)
 }
