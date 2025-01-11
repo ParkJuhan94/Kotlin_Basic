0df74d6062c5f94dafe1b00b50bb4cfe1a6dfc00
ParkJuHan94 <1004sunye@naver.com>
2025-01-12 07:37:38 +0900

    반복문 의 예제

--- src/KOTLIN_BASIC/Main_09.kt
+++ src/KOTLIN_BASIC/Main_09.kt
@@ -1,5 +1,18 @@
 package KOTLIN_BASIC
 
 fun main() {
+    val items = listOf(1, 2, 3, 4, 5)
 
+    for (item in items) {
+        println(item)
+    }
+
+    items.forEach { item ->
+        println(item)
+    }
+
+    // for (int i = 0; i <= 3; i++)
+    for(i in 0..3) {
+        println(items[i])
+    }
 }
