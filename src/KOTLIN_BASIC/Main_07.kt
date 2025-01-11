2afb575258ba3282432a0ac09c54952264d0b2f9
ParkJuHan94 <1004sunye@naver.com>
2025-01-12 03:42:35 +0900

    조건문 의 예제

--- src/KOTLIN_BASIC/Main_07.kt
+++ src/KOTLIN_BASIC/Main_07.kt
@@ -1,5 +1,13 @@
 package KOTLIN_BASIC
 
 fun main() {
+    var i = 5
 
+    var result = when {
+        i > 10 -> "10 보다 크다."
+        i > 5 -> "5 보다 크다."
+        else -> "5 이하이다."
+    }
+
+    println(result)
 }
