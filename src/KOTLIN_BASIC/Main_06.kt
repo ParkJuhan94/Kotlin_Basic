975f058e45ed0e33cc693deb7f09709bfca9887c
ParkJuHan94 <1004sunye@naver.com>
2025-01-12 03:42:12 +0900

    커맨드입력 의 예제

--- src/KOTLIN_BASIC/Main_06.kt
+++ src/KOTLIN_BASIC/Main_06.kt
@@ -1,5 +1,9 @@
 package KOTLIN_BASIC
 
-fun main() {
+import java.util.*
 
+fun main() {
+    val reader = Scanner(System.`in`)   // in은 코틀린에서 사용할 수 없는 키워드. 자동으로 ` ` 붙음.
+    val a = reader.nextInt()
+    val b = reader.next()
 }
