e8bf5475bf00646b1deb134671fdf36a2374b30d
ParkJuHan94 <1004sunye@naver.com>
2025-01-12 10:11:39 +0900

    Try & Catch 의 예제

--- src/KOTLIN_BASIC/Main_11.kt
+++ src/KOTLIN_BASIC/Main_11.kt
@@ -1,5 +1,12 @@
 package KOTLIN_BASIC
 
 fun main() {
+    val items = listOf(1, 2, 3)
 
+    // 출력: Index 4 out of bounds for length 3
+    try {
+        val item = items[4]
+    } catch (e: Exception) {
+        println(e.message)
+    }
 }
