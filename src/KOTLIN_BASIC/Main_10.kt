42b39615ae43cdd1cf3ea19157a95714bbf555d9
ParkJuHan94 <1004sunye@naver.com>
2025-01-12 10:11:08 +0900

    List, Array 의 예제

--- src/KOTLIN_BASIC/Main_10.kt
+++ src/KOTLIN_BASIC/Main_10.kt
@@ -1,5 +1,20 @@
 package KOTLIN_BASIC
 
 fun main() {
+    val items1 = listOf(1, 2, 3, 4, 5)   // 변경 불가
+    // mutableListOf<Int>(1, 2, 3, 4, 5) : <Int> 는 타입추론이 있기 때문에 생략 가능
+    val items2 = mutableListOf(1, 2, 3, 4, 5)   // 변경 가능
 
+    items2.add(6)
+    items2.remove(3)
+    items2[0] = 100
+
+    // 출력 : 100 2 3 4 5 6
+    for (item in items2) {
+        println(item)
+    }
+
+    val items3 = arrayOf(1, 2, 3)
+    println(items3.size)
+    items3[0] = 10
 }
