343968d676557564f5258a29d24effca299f357d
ParkJuHan94 <1004sunye@naver.com>
2025-01-12 11:12:35 +0900

    class 의 예제

--- src/KOTLIN_BASIC/Main_14.kt
+++ src/KOTLIN_BASIC/Main_14.kt
@@ -1,5 +1,15 @@
 package KOTLIN_BASIC
 
 fun main() {
+    val john = Person("John", 25)
+    println("name: ${john.name}")
 
+    // setter 사용하지 않아도 직접 값 변경 가능
+    john.age = 26
+    println("age: ${john.age}")
 }
+
+class Person(
+    val name: String,
+    var age: Int,
+)
