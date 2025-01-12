6bd999eb4d276a783d019fbd6c4fcce8e8470ff8
ParkJuHan94 <1004sunye@naver.com>
2025-01-12 10:49:04 +0900

    함수 의 예제 - 1

--- src/KOTLIN_BASIC/Main_13.kt
+++ src/KOTLIN_BASIC/Main_13.kt
@@ -1,5 +1,14 @@
 package KOTLIN_BASIC
 
 fun main() {
-
+    println(sum(10, 20))
 }
+
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
+
+//fun sum(a: Int, b: Int): Int = a + b
+
+
+//fun sum(a: Int, b: Int) = a + b
