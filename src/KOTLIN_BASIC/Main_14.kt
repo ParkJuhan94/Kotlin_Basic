6fe4a41d8775d73a32bdec55d88169bd6339553c
ParkJuHan94 <1004sunye@naver.com>
2025-01-12 11:13:32 +0900

    data class 의 예제

--- src/KOTLIN_BASIC/Main_14.kt
+++ src/KOTLIN_BASIC/Main_14.kt
@@ -2,7 +2,7 @@ package KOTLIN_BASIC
 
 fun main() {
     val john = Person("John", 25)
-    println("name: ${john.name}")
+    println("name: ${john.name}")   // 프로퍼티 접근 불가능
 
     // setter 사용하지 않아도 직접 값 변경 가능
     john.age = 26
@@ -10,6 +10,6 @@ fun main() {
 }
 
 class Person(
-    val name: String,
+    private val name: String,
     var age: Int,
 )
