faa3278204483ee653953197a754caf24be2d3d9
ParkJuHan94 <1004sunye@naver.com>
2025-01-14 04:45:17 +0900

    콜백함수(고차함수) 의 예제 추가

--- src/KOTLIN_BASIC/Main_19.kt
+++ src/KOTLIN_BASIC/Main_19.kt
@@ -1,5 +1,19 @@
 package KOTLIN_BASIC
 
 fun main() {
-
+    myFunc {
+        println("함수 호출")
+    }
 }
+
+fun myFunc(callBack : () -> Unit) {
+    println("함수 Start")
+    callBack()
+    println("함수 End")
+}
+
+/*
+함수 Start
+함수 호출
+함수 End
+ */
