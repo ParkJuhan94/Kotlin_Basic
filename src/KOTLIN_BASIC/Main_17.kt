de5d0690a23ec4d822b385b2a7d296b93fca987b
ParkJuHan94 <1004sunye@naver.com>
2025-01-14 04:26:15 +0900

    강제 타입 변환 as 의 예제 추가

--- src/KOTLIN_BASIC/Main_17.kt
+++ src/KOTLIN_BASIC/Main_17.kt
@@ -1,5 +1,34 @@
 package KOTLIN_BASIC
 
-fun main() {
-
+interface Drawable1 {
+    fun draw() {
+        println("Drawing")
+    }
 }
+
+interface Printable {
+    fun print() {
+        println("Printing")
+    }
+}
+
+class Document : Drawable1, Printable {
+    override fun draw() {
+        println("Document Drawing")
+    }
+
+    override fun print() {
+        println("Document Printing")
+    }
+}
+
+fun main() {
+    val drawable1: Drawable1 = Document()
+
+    // 강제 타입 변환 사용
+    val doc = drawable1 as Document
+
+    doc.draw()  // 출력: Document Drawing
+    doc.print() // 출력: Document Printing
+}
+
