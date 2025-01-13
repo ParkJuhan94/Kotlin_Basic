32dd1d80a24fa4a5dac3c0c08d64d0e4135cb85e
ParkJuHan94 <1004sunye@naver.com>
2025-01-14 03:05:02 +0900

    interface 의 예제 추가

--- src/KOTLIN_BASIC/Main_16.kt
+++ src/KOTLIN_BASIC/Main_16.kt
@@ -3,3 +3,29 @@ package KOTLIN_BASIC
 fun main() {
 
 }
+
+interface Drawable {
+    fun draw()
+}
+
+abstract class Animal {
+    open fun move() {
+        println("이동")
+    }
+}
+
+class Dog : Animal(), Drawable {
+    override fun move() {
+        println("껑충")
+    }
+
+    override fun draw() {
+        TODO("Not yet implemented")
+    }
+}
+
+class Cat : Animal() {
+    override fun move() {
+        println("살금")
+    }
+}
