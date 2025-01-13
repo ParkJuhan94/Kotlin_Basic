54e6f773f664f2c40484e4fcef5f8a33b4bc956f
ParkJuHan94 <1004sunye@naver.com>
2025-01-14 04:44:52 +0900

    제네릭클래스 의 예제 추가

--- src/KOTLIN_BASIC/Main_18.kt
+++ src/KOTLIN_BASIC/Main_18.kt
@@ -1,5 +1,13 @@
 package KOTLIN_BASIC
 
 fun main() {
+    val box1 = Box(10)          // Box<Int> 타입의 인스턴스 생성
+    val box2 = Box("apple")     // Box<String> 타입의 인스턴스 생성
 
+    println(box1.value)         // 출력: 10
+    println(box2.value)         // 출력: apple
+}
+
+class Box<T>(var value: T) {
+    // 제네릭 클래스 정의
 }
